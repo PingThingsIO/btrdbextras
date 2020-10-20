5d4fca53d534ec56e222e1d0d7ed8e440f3d0c08
looselycoupled <allen.leis@gmail.com>
2020-10-20 10:45:40 -0400

    wip

--- eventproc/conn.py
+++ eventproc/conn.py
@@ -23,34 +23,23 @@ import uuid as uuidlib
 import grpc
 from grpc._cython.cygrpc import CompressionAlgorithm
 
-INTERNAL_PORT="7598"
+PATH_PREFIX="/eventproc"
 
 
-def connect(endpoint=os.environ.get("BTRDB_ENDPOINTS"), apikey=os.environ.get("BTRDB_APIKEY")):
+def connect(endpoint=os.environ.get("BTRDB_ENDPOINTS"), apikey=os.environ.get("BTRDB_API_KEY")):
     addrport = endpoint.split(":", 2)
-    chan_ops = [] #[('grpc.default_compression_algorithm', CompressionAlgorithm.gzip)]
+    endpoint += PATH_PREFIX
 
     if len(addrport) != 2:
         raise ValueError("expecting address:port")
 
-    if addrport[1] == "4411":
+    if apikey is None or apikey == "":
+        raise ValueError("must supply an API key")
 
-        if apikey is None:
-            channel = grpc.secure_channel(
-                endpoint,
-                options=chan_ops
-            )
-        else:
-            channel = grpc.secure_channel(
-                endpoint,
-                grpc.composite_channel_credentials(
-                    grpc.access_token_call_credentials(apikey)
-                ),
-                options=chan_ops
-            )
-    else:
-        if apikey is not None and addrport[1] != INTERNAL_PORT:
-            raise ValueError("cannot use an API key with an insecure (port 4410) BTrDB API. Try port 4411")
-        channel = grpc.insecure_channel(endpoint, chan_ops)
-
-    return channel
+    return grpc.secure_channel(
+        endpoint,
+        grpc.composite_channel_credentials(
+            grpc.ssl_channel_credentials(None),
+            grpc.access_token_call_credentials(apikey)
+        )
+    )
--- eventproc/eventproc.py
+++ eventproc/eventproc.py
@@ -1,12 +1,24 @@
 import functools
+from collections import namedtuple
 
 from eventproc.conn import connect
 from eventproc.protobuff import api_pb2
 from eventproc.protobuff import api_pb2_grpc
 
+from btrdb.utils.timez import ns_to_datetime
 
 
+HandlerBase = namedtuple("HandlerBase", "id name version notify_on_success notify_on_failure flags created_at created_by updated_at updated_by")
 
+class Handler(HandlerBase):
+
+    @classmethod
+    def from_grpc(cls, h):
+        return cls(
+            h.id, h.name, h.version, h.notify_on_success, h.notify_on_failure,
+            h.flag, ns_to_datetime(h.created_at), h.created_by, 
+            ns_to_datetime(h.updated_at), h.updated_by
+        )
 
 class Service(object):
 
@@ -18,12 +30,32 @@ class Service(object):
         params = api_pb2.ListHooksRequest()
         return [r.name for r in self.stub.ListHooks(params).hooks]
 
-    def ListHandlers(self):
-        params = api_pb2.ListHandlersRequest()
-        for result in self.stub.ListHandlers(params):
+    def ListHandlers(self, hook):
+        params = api_pb2.ListHandlersRequest(hook=hook)
+        response = self.stub.ListHandlers(params)
+
+        for result in response.handlers:
             yield result
 
+    def Register(self, name, hook, apikey, notify_on_success, notify_on_failure, flags):
+        params = api_pb2.RegisterRequest(
+            registration=api_pb2.Registration(
+            name=name, 
+            hook=hook, 
+            api_key=apikey, 
+            notify_on_success=notify_on_success, 
+            notify_on_failure=notify_on_failure, 
+            flags=flags,        
+            )    
+        )
+        response = self.stub.Register(params)
+        if hasattr(response, "handler"):
+            return Handler.from_grpc(response.handler)
 
+    def Deregister(self, handler_id):
+        params = api_pb2.DeregisterRequest(id=handler_id)
+        response = self.stub.Deregister(params)
+        return response
 
 
 def hooks():
@@ -31,21 +63,33 @@ def hooks():
     s = Service(conn)
     return s.ListHooks()
 
-
-# def register(func):
-
-#     @functools.wraps(func)
-#     def wrapper(*args, **kwargs):
-#         func(*args, **kwargs)
-#         return func(*args, **kwargs)
-#     return wrapper
+def list_handlers(hook=""):
+    # call grpc service to register event handler
+    s = Service(connect())
+    return [Handler.from_grpc(h) for h in s.ListHandlers(hook)]
 
 
-# def foo(x=1):
-#     def wrap(f):
-#         def f_foo(*args, **kw):
-#             # do something to f
-#             return f(*args, **kw)
-#         return f_foo
-#     return wrap
+def deregister(handler_id):
+    # call grpc service to register event handler
+    s = Service(connect())
+    return s.Deregister(handler_id)
+    
 
+
+def register(name, hook, apikey, notify_on_success, notify_on_failure, flags=[]):
+    """
+    decorator to submit (register) an event handler function
+    """
+
+    # inner will actually receive the decorated func but we still have access
+    # to the args & kwargs due to closure/scope.
+    def inner(func):
+
+        # call grpc service to register event handler
+        s = Service(connect())
+        _ = s.Register(name, hook, apikey, notify_on_success, notify_on_failure, flags)
+
+        # return original func back to user
+        return func
+
+    return inner
